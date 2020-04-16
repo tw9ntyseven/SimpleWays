a5fb3ecec3c0e1f4a10391d2696f87c0beb51a67
tw9ntyseven <50303876+tw9ntyseven@users.noreply.github.com>
2020-04-16 13:16:37 +0300

    Updates

---
+++ precache-manifest.88cc98188e9f0a2e99d50c95d2bd2b26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beeff25a4e3ffc37a02a09be2dedd538",
+    "url": "/SimpleWays/index.html"
+  },
+  {
+    "revision": "7884c8b2635c7333705c",
+    "url": "/SimpleWays/static/css/main.f3c45046.chunk.css"
+  },
+  {
+    "revision": "ac49bbbe46e189b9d5dc",
+    "url": "/SimpleWays/static/js/2.9d96288b.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/SimpleWays/static/js/2.9d96288b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7884c8b2635c7333705c",
+    "url": "/SimpleWays/static/js/main.539a2a73.chunk.js"
+  },
+  {
+    "revision": "1dd2ae4a5c7ad491236b",
+    "url": "/SimpleWays/static/js/runtime-main.acb02414.js"
+  }
+]);
