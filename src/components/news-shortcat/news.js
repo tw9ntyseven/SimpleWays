41b6f7387b65c091aef9d5822a23b84f10845467
tw9ntyseven <vladakmilicin@mail.ru>
2020-04-27 13:23:16 +0300

    add loader-spinner

--- src/components/news-shortcat/news.js
+++ src/components/news-shortcat/news.js
@@ -1,13 +1,28 @@
 import './news.css';
 import React, { Component } from 'react';
 
+import NewsItem from '../newsItem/newsItem';
+
 export class newsShortcat extends Component {
     render() {
         return (
             <div className="news-wrapper">
                 <h2 className="news-title">Новости</h2>
                 <div className="news-block">
-                {/* <i class="fab fa-react" style={{fontSize: "100px"}}></i> */}
+                <NewsItem className="newsBlockItems"
+                items={[
+                         {
+                             link: '/corona',
+                             title: "Главное о COVID-19",
+                             subtitle: "Единая горячая линия:8-800-2000-112 Основные меры предосторожности для защиты от новой коронавирусной инфекции:",
+                         },
+                         {
+                             link: '/ways',
+                             title: "Маршруты города Симферополя",
+                             subtitle: "Узнайте больше о маршрутах города Симферополя, а также изучайте путь движения полюбившегося транспорта.",
+                         },
+                ]}
+                 />
                 </div>
             </div>
         );
--- src/pages/maps/maps.js
+++ src/pages/maps/maps.js
@@ -2,30 +2,37 @@ import React, {Component} from 'react';
 import './maps.css';
 import HeadBar from '../../components/header-bar/header-bar';
 import {YMaps, Map, Placemark} from "react-yandex-maps";
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 import Footer from '../../components/footer/footer';
+
 const mapData = {
     center: [
         44.948314, 34.100156
     ],
     zoom: 12
 };
+const antIcon = <LoadingOutlined style={{ fontSize: 64, color: "#2039b3", display: "flex", justifyContent: "center", alignItems: "center" }} spin />;
 
 class Maps extends Component {
+    state = {
+        loaded: false,
+    }
+    onMapLoaded = () => {
+        this.setState({ loaded : true });
+    }
     render() {
+        const {loaded} = this.state;
         return (
             <div>
                 <HeadBar/>
                 <div style={{display: "block", width: "100%", height: "95vh", marginTop: "50px"}}>
-                <YMaps
-                     query={{
-                     apikey: '1c80c4b3-7cec-4d86-8f33-0c2061e5f814',
-                    }}>
-                    <Map defaultState={mapData}
-                     style={{width: "100%", height: "95vh", marginTop: "50px"}}
-                     >
-                    </Map> 
-                </YMaps>
+                { !loaded &&  <YMaps query={{apikey: '1c80c4b3-7cec-4d86-8f33-0c2061e5f814'}} onLoad={this.onMapLoaded}>
+                    <Map defaultState={mapData} style={{width: "100%", height: "95vh", marginTop: "50px"}}>
+                    </Map>
+                    <Spin indicator={antIcon} />
+                </YMaps>}
                 </div>
                 <Footer/>
             </div>
